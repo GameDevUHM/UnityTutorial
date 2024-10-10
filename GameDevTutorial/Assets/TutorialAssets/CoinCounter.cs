e9d80b0c26965a04c49a61b31b82dba8475e3547
jialia <92237586+jiahuiliao@users.noreply.github.com>
2024-10-10 13:47:07 -1000

    Add Comments
    Comments added to scripts

--- GameDevTutorial/Assets/TutorialAssets/CoinCounter.cs
+++ GameDevTutorial/Assets/TutorialAssets/CoinCounter.cs
@@ -5,15 +5,22 @@ using TMPro;
 
 public class CoinCounter : MonoBehaviour
 {
+    //int variable to store coin count
     public int coins = 0;
+    //TMP_Text variable stores the text to be changed
     public TMP_Text coinCount;
 
+    //OnTriggerEnter is called when two game objects collide
     private void OnTriggerEnter2D(Collider2D other)
     {
+        //Specifically, looking for collisions with objects tagged "coin"
         if(other.gameObject.CompareTag("Coin"))
         {
+            //the coin will be destroyed
             Destroy(other.gameObject);
+            //the coin count is incremented
             coins++;
+            //and the text displayed is updated
             coinCount.text = "Coins: " + coins.ToString();
         }
     }
--- GameDevTutorial/Assets/TutorialAssets/PlayerController.cs
+++ GameDevTutorial/Assets/TutorialAssets/PlayerController.cs
@@ -4,22 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //float variable moveSpeed determines speed of movement
     public float moveSpeed = 3f;
+    //Rigidbody2D component allows player to move
     public Rigidbody2D rb;
+    //Stores X and Y user input
     Vector2 movementDirection;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Gets component of type Rigidbody2D on player gameobject
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //assigning horizontal (x) and vertical (y) user input to movementDirection
         movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
     }
 
+    //Use FixedUpdate in case of physics (for rigidbody)
     void FixedUpdate()
     {
         rb.velocity = movementDirection * moveSpeed;
